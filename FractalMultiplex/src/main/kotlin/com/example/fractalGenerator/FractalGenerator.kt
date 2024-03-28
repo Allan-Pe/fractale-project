bdccd72ae8984b27c358a6277a203423f96d53e8
Allan Pereira <allan.pereira@edu.itescia.fr>
2024-03-28 15:52:53 +0100

    modif zoom

--- FractalMultiplex/src/main/kotlin/com/example/fractalGenerator/FractalGenerator.kt
+++ FractalMultiplex/src/main/kotlin/com/example/fractalGenerator/FractalGenerator.kt
@@ -51,11 +51,11 @@ class FractalGenerator {
 
     fun updateFractalPosition(direction: String) {
         when (direction) {
-            "left" -> this.centerX -= 1
-            "right" -> this.centerX += 1
+            "left" -> this.centerX += 1
+            "right" -> this.centerX -= 1
             "up" -> this.centerY += 1
             "down" -> this.centerY -= 1
-            "zoomin" -> this.scale /= 1
+            "zoomin" -> this.scale /= 2
             "zoomout" -> this.scale *= 2
             else -> println("No valid direction !")
         }
