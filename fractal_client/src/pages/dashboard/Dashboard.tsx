4c89aa8072b4cd9c669aa44c237e9db6c1726a26
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2024-03-28 14:40:34 +0100

    setting movement

--- fractal_client/src/pages/dashboard/Dashboard.tsx
+++ fractal_client/src/pages/dashboard/Dashboard.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
-import { apiCall } from "../../services/service";
+import { generateFractal, updateFractalPosition } from "../../services/service";
 
 const Dashboard = () => {
   const [fractalImg, setFractalImg] = useState<string>();
 
   const testGet = async () => {
     try {
-      const response: any = await apiCall();
+      const response: any = await generateFractal();
 
       if (!(response instanceof Blob)) {
         throw new Error("Response is not a Blob.");
@@ -23,6 +23,52 @@ const Dashboard = () => {
       console.error("Error:", error);
     }
   };
+  const sendMovement = async (direction: string) => {
+  
+    const updateFractalResponse: any = await updateFractalPosition(direction)
+    if (!(updateFractalResponse instanceof Blob)) {
+      throw new Error("Response is not a Blob.");
+    }
+    const url = URL.createObjectURL(updateFractalResponse);
+    console.log(url);
+    setFractalImg(url);
+  }
+
+  useEffect(() =>{
+    const handleKeyDown = (event: any) => {
+      switch (event.key){
+        case 'd':
+          sendMovement("right");
+          break;
+        case 'q':
+          sendMovement("left");
+          break;
+        case 'z':
+          sendMovement("up");
+          break;
+        case 's':
+          sendMovement("down")
+          break;
+        case 'a':
+          sendMovement("zoomin")
+          break;
+        case 'e':
+          sendMovement("zoomout")
+          break
+        default:
+          break;
+
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    };
+
+  }, []);
+
 
   return (
     <Box>
--- fractal_client/src/services/service.ts
+++ fractal_client/src/services/service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Make a request for a user with a given ID
-export const apiCall = async () => {
+export const generateFractal = async () => {
   try {
     const response: any = await axios.get(
       "http://localhost:8080/generatefractal",
@@ -15,3 +15,22 @@ export const apiCall = async () => {
     console.log(error);
   }
 };
+
+export const updateFractalPosition = async (direction: string) => {
+  const config = {
+    body: {
+      direction
+    }
+  }
+
+  try {
+    await axios.post("http://localhost:8080/generatefractal", config)
+    .then(response => {
+      const responseBlob = new Blob([response.data], { type: "image/jpeg" });
+      console.log(responseBlob)
+    })
+
+  } catch (error) {
+    
+  }
+}
