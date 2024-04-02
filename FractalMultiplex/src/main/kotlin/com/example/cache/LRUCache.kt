a8aa53ebbd9c8458e2cdd98bff47d295feeda230
Thibault Blanchard <thibault.blanchard@edu.itescia.fr>
2024-04-02 14:13:28 +0200

    lru cache done?

--- FractalMultiplex/src/main/kotlin/com/example/cache/LRUCache.kt
+++ FractalMultiplex/src/main/kotlin/com/example/cache/LRUCache.kt
@@ -1,5 +1,8 @@
 package com.example.cache
 
+import io.pebbletemplates.pebble.node.CacheNode
+
+
 interface LRUCache<V> {
     fun get(key: Int): V?
     fun put(key: Int, value: V)
@@ -8,32 +11,32 @@ interface LRUCache<V> {
 }
 
 
-class Cache(val capacity: Int) : LRUCache<String> {
-    var cache = LinkedHashMap<Int, String>(capacity, 0.75f, true)
+class Cache<T>(val capacity: Int) : LRUCache<T> {
+    var cache = LinkedHashMap<Int, T>(capacity, 0.75f, true)
 
-    override fun get(key: Int): String? {
+    override fun get(key: Int): T? {
         return cache[key]
     }
 
-    fun getLastElement(): String? {
+    fun getLastElement(): T? {
         return cache.entries.lastOrNull()?.value
     }
 
-    fun getElementBefore(key: Int): String?{
+    fun getElementBefore(key: Int): T?{
         if (cache.size >= 2){
-            return cache.entries.elementAt(key - 1).value
+            return cache.entries.elementAt(cache.size - 2).value
         }
         return null
     }
 
-    fun getElementAfter(key: Int): String?{
-        if (cache.size >= 2){
+    fun getElementAfter(key: Int): T?{
+        if (cache.size >= 2 || key+1 < cache.size){
             return cache.entries.elementAt(key + 1).value
         }
         return null
     }
 
-    override fun remove(key: Int): String? {
+    override fun remove(key: Int): T? {
         return cache.remove(key)
     }
 
@@ -41,27 +44,14 @@ class Cache(val capacity: Int) : LRUCache<String> {
         cache.clear()
     }
 
-    override fun put(key: Int, value: String) {
-
+    override fun put(key: Int, value: T) {
         if (cache.size >= capacity) {
+            println(cache.keys.iterator().next())
             val leastUsedKey = cache.keys.iterator().next()
 
             cache.remove(leastUsedKey)
         }
         cache[key] = value
-//        val oldestKey = 1
-//
-//        if(cache.isEmpty()) {
-//            cache[1] = value
-//        }
-//        else if (cache.size + 1 <= capacity){
-//            cache.put(cache.size+1, value)
-//        }
-//        else {
-//            cache.remove(oldestKey)
-//            cache[oldestKey] = value
-//            cache.toSortedMap()
-//    }
     }
 
     override fun toString(): String {
@@ -73,17 +63,21 @@ class Cache(val capacity: Int) : LRUCache<String> {
 
 fun main() {
 
-    val cache = Cache(3)
+    val cache = Cache<String>(3)
+    cache.clear()
 
     cache.put( 1, "a")
     cache.put( 2, "b")
-//    cache.put( 3, "c")
-//    cache.put( 4, "d")
-//    cache.put( 5, "e")
-//    cache.put( 6, "f")
+    cache.put( 3, "c")
 
-    println(cache.getElementBefore(1))
-    println(cache.getElementAfter(0))
+    cache.put( 4, "d")
+    cache.put( 5, "e")
+    cache.put( 6, "f")
+    cache.put( 7, "toto")
+
+    println(cache.getLastElement())
     println(cache.toString())
+    println(cache.getElementBefore(2))
+    println(cache.getElementAfter(1))
 
 }
